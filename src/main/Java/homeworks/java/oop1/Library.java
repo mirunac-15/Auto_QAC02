db97158534c32926f051ebbc7bdb530bec1c3b98
mirunacondac <miruna.condac@yahoo.com>
2024-08-07 12:13:46 +0300

    Curs 8 homework- Library (Book/Author)

--- src/main/Java/homeworks/java/oop1/Library.java
+++ src/main/Java/homeworks/java/oop1/Library.java
@@ -9,8 +9,10 @@ public class Library {
         Book book2 = new Book("Maitreyi", 1933, author2, 35);
 
 
-        System.out.println("Book 1: " + book1.getName() + " are pretul " + book1.getPrice() + " de RON, este scrisa de autorul " + author1.getName() + " si a fost publicata in anul " + book1.getYear() + ".");
-        System.out.println("Book 2: " + book2.getName() + " are pretul " + book2.getPrice() + " de RON, este scrisa de autorul " + author2.getName() + " si a fost publicata in anul " + book2.getYear() + ".");
+        System.out.println(book1.scriecarte());
+        System.out.println(book2.scriecarte());
+
+
 
 
 
